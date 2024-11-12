6842b007750d9fd2c6c4e5e7e6e602cce4a07fe1
AndyRP1 <0960247@my.scccd.edu>
2024-11-12 11:54:34 -0800

    Add files via upload

---
+++ DemoTues12Application.java
@@ -0,0 +1,13 @@
+package com.andy.demoTues12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTues12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTues12Application.class, args);
+	}
+
+}
