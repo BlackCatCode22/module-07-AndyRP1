0fd3ffd3eac98748d8d64ba1387424d41a77e7d6
AndyRP1 <0960247@my.scccd.edu>
2024-11-19 12:01:47 -0800

    Add files via upload

---
+++ ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.tuesday.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
